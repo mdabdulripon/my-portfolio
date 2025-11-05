76aad988efa6d1f36181003137ef48602fd13561
mdabdulripon <mdabdulripon@gmail.com>
2025-11-05 12:09:36 -0600

    Add link to logo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar.js";
+import { Link } from "react-router-dom";
 
 export default function Intro() {
 	return (
 		<div className="profile">
-			<h1>Abdul Awal Ripon</h1>
+			<Link className="logo" to="/">Abdul Awal Ripon</Link>
 			<div className="title">Lead Software Engineer</div>
 			<p className="summary">
 				Experienced full-stack engineer (10+ years) across finance, insurance,
 				education, and payments. I design and deliver scalable, high-performing
 				web and cloud solutions, mentor teams, and drive continuous improvement.
 			</p>
-
 			<Navbar />
 		</div>
 	);
